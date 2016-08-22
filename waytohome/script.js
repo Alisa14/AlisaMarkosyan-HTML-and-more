5b9a5cb6f548504330f3519eb3ee00857cd887dc
Alisa14 <alisamarkosyan@gmail.com>
2016-08-22 22:07:50 +0400

    Way to home

---
+++ waytohome/script.js
@@ -0,0 +1,21 @@
+function sampleFunction() {
+  var stack = [];
+  stack.push("Moxraguyn 2 mutqani shenq");       
+  stack.push("16 harkani 6-ankyun shenq" );  
+  stack.push("erkar 7 mutqov 9 harkani");       
+  stack.push("3 harkani shenq, araji harkum bank");
+ for (i = 0; i < stack.length; i++) { 
+    alert(stack[i];)
+}
+}
+
+
+
+
+
+
+var calculate = function(){
+    var age=(document.getElementById("age").value);
+    var result = 
+   
+}
